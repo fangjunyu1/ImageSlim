663ec5d156dc12f270b3592c948071ab8e142873
方君宇 <fangjunyu.com@gmail.com>
2025-06-18 12:35:37 +0800

    创建轻压图片Xcode项目

---
+++ ImageSlim/ImageSlimApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSlimApp.swift
+//  ImageSlim
+//
+//  Created by 方君宇 on 2025/6/18.
+//
+
+import SwiftUI
+
+@main
+struct ImageSlimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
